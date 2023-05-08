a91a67531c2c4ae471f3fc14ccad0da2852a3598
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-05-08 16:34:46 +0530

    cleared with conflicts

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,5 @@
-const express=require("express")
-const {connection}=require("./database/db");
-const {router}=require("./controller/user.rout")
-const {authenticate}=require("./middleware/auth")
-//....
-
+const express = require("express");
+const app = express();
 const socketio = require("socket.io");
 
 const mongoose = require("mongoose");
@@ -11,16 +7,10 @@ var randomId = require("random-id");
 const { User, update_word_function } = require("./user");
 let { users } = require("./user");
 let cors = require("cors");
-require("dotenv").config()
-const app = express();
+let { connection } = require("./db")
+
 app.use(cors());
 app.use(express.json());
-
-app.get("/",authenticate,(req,res)=>{
-    res.send("wellcome")
-});
-app.use("/user",router)
-
 
 
 
@@ -30,131 +20,137 @@ var len = 10;
 // default is aA0 it has a chance for lowercased capitals and numbers
 var pattern = "aA0";
 
+const expressServer = app.listen(8080, async () => {
+  try {
+    await connection
+    console.log("connected to db");
+  } catch (error) {
+    console.log(error.message);
+  }
 
-const expressServer = app.listen(process.env.PORT, async () => {
-    try {
-      await connection
-      console.log("connected to db");
-    } catch (error) {
-      console.log(error.message);
-    }
-  
-    console.log(`${process.env.PORT}`);
-  });
+  console.log("server running");
+});
 
-  const io = socketio(expressServer);
 
-  //my global array or clients
+const io = socketio(expressServer);
+
+// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>//
+// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>//
+// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>//
+
+//my global array or clients
 // let arr = [];
 
 // here is my ranodom paragraph
 let para = [
-    "So, I was essentially a backbencher or some kind ofoutcast "
-  ];
-  // So, I was essentially a backbencher or some kind of outcast there. Feeling a bit dejected, I wanted to prove something my worth to them. So, I started to wait for an opportunity.
-  function myFunction() {
-    let random = Math.floor(Math.random() * para.length);
-    return random;
-  }
-  
-  //on connect
-  let count = 0;
-  let totalWords = 0;
-  
-  io.on("connection", (socket) => {
-    count += 1;
-  
-    socket.on("username", ({ username }) => {
-  
-      var id = randomId(len, pattern);
-      console.log(id);
-      socket.emit("roomno", id);
-    });
+  "So, I was essentially a backbencher or some kind ofoutcast "
+];
+// So, I was essentially a backbencher or some kind of outcast there. Feeling a bit dejected, I wanted to prove something my worth to them. So, I started to wait for an opportunity.
+function myFunction() {
+  let random = Math.floor(Math.random() * para.length);
+  return random;
+}
+
+//this is js file..
+
+//on connect
+let count = 0;
+let totalWords = 0;
+
+io.on("connection", (socket) => {
+  count += 1;
+
+  socket.on("username", ({ username }) => {
+
+    var id = randomId(len, pattern);
+    console.log(id);
+    socket.emit("roomno", id);
+  });
   let Room;
-    socket.on("joinroom", ({ username, roomvalue }) => {
-      const user = User(socket.id, username, roomvalue);
-      console.log(roomvalue + "from join room");
-      console.log(socket.id + "from line no 68");
-      socket.join(roomvalue);
-      Room=roomvalue;
-      io.emit("usersarray", users)
-      socket.emit("message", "WELCOME TO RACE BUDDY 😉");
-    });
-    console.log(`One user connected, total user : ${count}`);
-  
-    socket.on("timeleft", (data) => {
-     let {timeleft}=data;
-     socket.broadcast.to(Room).emit("Time",{timeleft})
-    });
-    io.emit('user count', count);
-  
-    socket.on("display", (data) => {
-      socket.broadcast.to(Room).emit("forall", data);
-    });
-  
-    //emitting the paragraph
-    let myParagraph = para[0];
-    socket.emit("thePara", myParagraph);
-  
-    //recieving the typed text from client
-    socket.on("typedText", ({ typedText }) => {
-      console.log(`person having id ${socket.id} is typing :`, typedText);
-  
-      if (
-        typedText[typedText.length - 1] == myParagraph[typedText.length - 1] &&
-        includeFunction(myParagraph, typedText)
-      ) {
-        if (typedText.length == myParagraph.length) {
-          console.log(typedText);
-          // users = []
-          return socket.emit("typing-update", {
-            typedText: "You have finished the race buddy 👏👏👏",
-            flag: "Race Completed",
-          });
-        }
-        if (typedText[typedText.length - 1] == " ") {
-          let user = update_word_function(socket.id, typedText);
-          console.log(user);
-          console.log(user[0])
-          io.to(user[0].roomvalue).emit("user_data",
-            user[0],
-  
-          );
-        }
-        // console.log({ typedText, keyCode });
-        socket.emit("typing-update", {
-          typedText,
-  
-          isTyping: true,
-          socketID: socket.id,
-          flag: true,
-          totalWords,
-        });
-      } else {
-        socket.emit("typing-update", {
-          typedText,
-          isTyping: false,
-          socketID: socket.id,
-          flag: false,
-          totalWords,
+  socket.on("joinroom", ({ username, roomvalue }) => {
+    const user = User(socket.id, username, roomvalue);
+    console.log(roomvalue + "from join room");
+    console.log(socket.id + "from line no 68");
+    socket.join(roomvalue);
+    Room = roomvalue;
+    io.emit("usersarray", users)
+    socket.emit("message", "WELCOME TO RACE BUDDY 😉");
+  });
+  console.log(`One user connected, total user : ${count}`);
+
+  socket.on("timeleft", (data) => {
+    let { timeleft } = data;
+    socket.broadcast.to(Room).emit("Time", { timeleft })
+  });
+  io.emit('user count', count);
+
+  socket.on("display", (data) => {
+    socket.broadcast.to(Room).emit("forall", data);
+  });
+
+  //emitting the paragraph
+  let myParagraph = para[0];
+  socket.emit("thePara", myParagraph);
+
+  //recieving the typed text from client
+  socket.on("typedText", ({ typedText }) => {
+    console.log(`person having id ${socket.id} is typing :`, typedText);
+
+    if (
+      typedText[typedText.length - 1] == myParagraph[typedText.length - 1] &&
+      includeFunction(myParagraph, typedText)
+    ) {
+      if (typedText.length == myParagraph.length) {
+        console.log(typedText);
+        // users = []
+        return socket.emit("typing-update", {
+          typedText: "You have finished the race buddy 👏👏👏",
+          flag: "Race Completed",
         });
       }
-    });
-    //disconnet
-    socket.on("disconnect", () => {
-      count -= 1;
-      console.log(`One user left, ${count} remaining!!`);
-      io.emit('user count', count);
-    });
-  });
-  
-  /*Here I am checking includes */
-  const includeFunction = (myParagraph, typedText) => {
-    if (myParagraph.includes(typedText)) {
-      return true;
+      if (typedText[typedText.length - 1] == " ") {
+        let user = update_word_function(socket.id, typedText);
+        console.log(user);
+        console.log(user[0])
+        io.to(user[0].roomvalue).emit("user_data",
+          user[0],
+
+        );
+      }
+      // console.log({ typedText, keyCode });
+      socket.emit("typing-update", {
+        typedText,
+
+        isTyping: true,
+        socketID: socket.id,
+        flag: true,
+        totalWords,
+      });
     } else {
-      return false;
+      socket.emit("typing-update", {
+        typedText,
+        isTyping: false,
+        socketID: socket.id,
+        flag: false,
+        totalWords,
+      });
     }
-  };
-  
-  module.exports = { count }
+  });
+  //disconnet
+  socket.on("disconnect", () => {
+    count -= 1;
+    console.log(`One user left, ${count} remaining!!`);
+    io.emit('user count', count);
+  });
+});
+
+/*Here I am checking includes */
+const includeFunction = (myParagraph, typedText) => {
+  if (myParagraph.includes(typedText)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = { count }
