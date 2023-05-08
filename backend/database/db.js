cc11bc8c56981fa26790bdfab17f784108c27ce4
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-05-08 16:27:13 +0530

    Delete backend directory

--- backend/database/db.js
+++
@@ -1,3 +0,0 @@
-const mongoose=require("mongoose")
-const connection=mongoose.connect("mongodb+srv://khirod:khirodsamal@cluster0.yvhnl0b.mongodb.net/typeRacer?retryWrites=true&w=majority")
-module.exports={connection}
