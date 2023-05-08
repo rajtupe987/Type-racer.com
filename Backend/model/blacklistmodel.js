66941b67847a9f5ed56bb72099c96ebbe637db33
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-05-08 16:19:03 +0530

    Delete Backend directory

--- Backend/model/blacklistmodel.js
+++
@@ -1,8 +0,0 @@
-
-const mongoose=require("mongoose")
-const blacklistedTokenSchema=mongoose.Schema({
-    accessToken:{type:String,require:true},
-    refreshToken:{type:String,require:true},
-})
-const blacklistModel=mongoose.model("blacklistedtoken",blacklistedTokenSchema)
-module.exports={blacklistModel}
